c2ca854461c1a6907e1bf72f7fe7bd6568056ad8
Miguel Boschini <miguelboschini07@gmail.com>
2025-08-30 23:06:09 -0300

    .

--- services/__init__.py
+++ services/__init__.py
@@ -2,7 +2,6 @@ from .file_service import ler_arquivo
 from .openai_service import gerar_classificacao_e_resposta
 from .historico_service import (
     carregar_historico,
-    ordenar_historico,
     obter_ultima_classificacao,
     adicionar_email  
 )
